7fa2fe8bc64d4b7fa84ee2b91e62b2eff90a3f44
GeroT1 <gerotaboada2002@gmail.com>
2025-03-22 20:54:33 -0300

    Inicio del proyecto
    Se crearon los .py para definir la app y para configurar el api_key

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather_data = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        if city:
+            weather_data = get_weather(city, app.config["OPENWEATHER_API_KEY"])
+        else:
+            return redirect(url_for("index"))
+        
+    return render_template("index.html", weather = weather_data)
